d2244408da8b80c6d1c8d19f3d7a928e5f3fed54
Daniel Nachun <daniel.nachun@gmail.com>
2024-11-19 18:41:49 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,15 +10,17 @@ cargo-bundle-licenses \
 # build statically linked binary with Rust
 cargo install --bins --no-track --locked --root ${PREFIX} --path .
 
-procs --gen-completion bash
-procs --gen-completion fish
-procs --gen-completion zsh
-mkdir -p ${PREFIX}/etc/bash_completion.d
-mkdir -p ${PREFIX}/share/fish/vendor_completions.d
-mkdir -p ${PREFIX}/share/zsh/site-functions
-install -m 644 procs.bash ${PREFIX}/etc/bash_completion.d/procs.bash
-install -m 644 procs.fish ${PREFIX}/share/fish/vendor_completions.d/procs.fish
-install -m 644 _procs ${PREFIX}/share/zsh/site-functions/_procs
+if [[ ${build_platform} == ${target_platform} ]]; then
+    procs --gen-completion bash
+    procs --gen-completion fish
+    procs --gen-completion zsh
+    mkdir -p ${PREFIX}/etc/bash_completion.d
+    mkdir -p ${PREFIX}/share/fish/vendor_completions.d
+    mkdir -p ${PREFIX}/share/zsh/site-functions
+    install -m 644 procs.bash ${PREFIX}/etc/bash_completion.d/procs.bash
+    install -m 644 procs.fish ${PREFIX}/share/fish/vendor_completions.d/procs.fish
+    install -m 644 _procs ${PREFIX}/share/zsh/site-functions/_procs
+fi
 
 # strip debug symbols
 "$STRIP" "$PREFIX/bin/${PKG_NAME}"
